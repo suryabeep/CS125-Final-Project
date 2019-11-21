8b62150f215f176aa9853be2bf22c9127bd2c261
Suryadip Bandyopadhyay <sband2000@gmail.com>
2019-11-21 15:48:17 -0600

    added example request

--- sous-chef/App.js
+++ sous-chef/App.js
@@ -1,6 +1,30 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+/* this is the stuff from the rapidAPI documentation on how to create an example request.
+
+
+var unirest = require("unirest");
+
+var req = unirest("GET", "https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/quickAnswer");
+
+req.query({
+	"q": "How much vitamin c is in 2 apples%3F"
+});
+
+req.headers({
+	"x-rapidapi-host": "spoonacular-recipe-food-nutrition-v1.p.rapidapi.com",
+	"x-rapidapi-key": "SIGN-UP-FOR-KEY"
+});
+
+
+req.end(function (res) {
+	if (res.error) throw new Error(res.error);
+
+	console.log(res.body);
+});
+*/
+
 export default function App() {
   return (
     <View style={styles.container}>
