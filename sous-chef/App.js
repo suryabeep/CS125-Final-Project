0571d098eaad70d35441902686b9ada7a72929fa
Suryadip Bandyopadhyay <sband2000@gmail.com>
2019-11-21 19:33:45 -0600

    minor corrections

--- sous-chef/App.js
+++ sous-chef/App.js
@@ -25,12 +25,14 @@ req.end(function (res) {
 });
 */
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+export default class App extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>Open up App.js to start working on your app!</Text>
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
